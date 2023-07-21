1e967e44af41366ea7d65c29f522fd4244ce6745
donndaryl <donndaryl@gmail.com>
2023-07-21 14:20:10 +0200

    /

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(
+    name="mega-calculator",
+    version="0.0.2",
+    description="add sub div min",
+    packages=["mega_calculator"],
+    package_dir={"": "src"}
+)
--- src/mega_calculator/__init__.py
+++ src/mega_calculator/__init__.py
@@ -1,1 +1,6 @@
+from parser import create_parser
+from operation import do_operation
+
+
+
 __version__ = "0.1.0"
