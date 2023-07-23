0eff5f03971752c557c6ca3702a1283d67b3cd57
donndaryl <donndaryl@gmail.com>
2023-07-23 13:05:02 +0200

    /

--- src/mega_calculator/operation.py
+++ src/mega_calculator/operation.py
@@ -1,4 +1,4 @@
-import math_
+import mathe
 import functools
 
 
--- tests/mega_calculator_tests/test_mathe.py
+++ tests/mega_calculator_tests/test_mathe.py
@@ -1,6 +1,6 @@
 from src.mega_calculator import __version__
 
-from src.mega_calculator import math_
+from src.mega_calculator import mathe
 
 
 def test_version():
@@ -8,16 +8,16 @@ def test_version():
 
 
 def test_add():
-    assert 15 == math_.add(10, 5)
+    assert 15 == mathe.add(10, 5)
 
 
 def test_sub():
-    assert 5 == math_.sub(10, 5)
+    assert 5 == mathe.sub(10, 5)
 
 
 def test_mul():
-    assert 50 == math_.mul(10, 5)
+    assert 50 == mathe.mul(10, 5)
 
 
 def test_div():
-    assert 2 == math_.div(10, 5)
+    assert 2 == mathe.div(10, 5)
